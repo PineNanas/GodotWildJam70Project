9a8c6799d71e5409ef55e2de9c5b06d1e05feae4
PineNanas <pinenanas@outlook.com>
2024-06-24 02:57:25 +0800

    final

--- Components/HurtBoxComponent/HurtBoxComponent.gd
+++ Components/HurtBoxComponent/HurtBoxComponent.gd
@@ -5,9 +5,19 @@ class_name HurtBox
 @onready var parent = get_parent()
 
 func give_hit(damage:int):
-	pass
+	if parent.has_method("take_damage"):
+		parent.take_damage(damage)
+	else:
+		var paparent = parent.get_parent()
+		if paparent.has_method("take_damage"):
+			paparent.take_damage(damage)
+		else:
+			pass
 
 
 
 func _give_hit(damage:int):
-	get_parent().give_hit()
+	if parent.has_method("give_hit"):
+		parent.give_hit()
+	else:
+		pass
